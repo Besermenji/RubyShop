df564eec17e2f688909820a06bd5f7a03490de30
besermenji <besermenji.vladimir@gmail.com>
2015-10-21 01:55:50 +0200

    enable user authentications and wrote methods that will check users role for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,36 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+
+  #method that check if user is logged in
+  def require_user
+    redirect_to "/login" unless current_user
+  end
+
+  #gets current users role names
+  private
+  def get_role_array
+    users_roles = UserRole.find_by(user_id: @current_user.id).to_a
+    role_names = [] 
+    users_roles.each do |x|
+      role_names << Role.find_by(id: x.role_id).role_name
+    end
+
+    role_names
+  end
+
+  def is_admin?
+    get_role_array.include? "admin"
+  end
+
+  def is_user?
+    get_role_array.include? "user"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+
+  resources :sessions
+  resources :users
+  get "/signup" => "users#new" 
+  resources :users 
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
