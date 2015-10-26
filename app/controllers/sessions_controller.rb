93f559ee5df4a72808a03d658ff001230a83e7f7
besermenji <besermenji.vladimir@gmail.com>
2015-10-27 00:09:07 +0100

    worked on login ajax

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,25 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:session][:email])
-    if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
-      redirect_to "/"
-    else
-      redirect_to "/login" 
-    end
+   # if @user && @user.authenticate(params[:session][:password])
+    #  session[:user_id] = @user.id
+   #   redirect_to "/"
+   # else
+    #  redirect_to "/login" 
+   # end
+    respond_to do |format|
+#   if @user && @user.authenticate(params[:session][:password])
+    # format.html{}
+     format.js {}
+    # format.json {}
+  
+     
+
+  end 
+#      format.html {redirect_to login_path}
+     # format.js {render :template => "new.js.erb"}
+     # format.html {render :template => "new.html.erb"}
+#    end
   end
 
   def destroy
